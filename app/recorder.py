fdf298cef342dfae995dc13f31407043a02359e0
drakrig <drakrig@gmail.com>
2025-01-07 18:40:43 +0300

    Add basic Recorder and VAD implementations

---
+++ app/recorder.py
@@ -0,0 +1,50 @@
+from queue import Queue
+import pyaudio
+from threading import Thread
+from logging import getLogger, basicConfig, INFO
+
+logger = getLogger(__name__)
+basicConfig(level=INFO)
+
+class Recorder():
+    def __init__(self,
+                 output_queue:Queue,
+                 sample_rate=16000,
+                 frame_duration_ms=32):
+        self.output_queue = output_queue
+        self.sr = sample_rate
+        self.frame_size = int(sample_rate * frame_duration_ms / 1000)
+        self.running = False
+    
+    def _stream_audio(self):
+        while self.running:
+            data = self.audio_stream.read(self.frame_size)
+            self.output_queue.put(data)
+    
+    def start_recording(self, device_index=None):
+        self.audio = pyaudio.PyAudio()
+        self.audio_stream = self.audio.open(format=pyaudio.paFloat32,
+                                       channels=1,
+                                       rate=self.sr,
+                                       input=True,
+                                       frames_per_buffer=self.frame_size,
+                                       input_device_index=device_index)
+        self.running = True
+        self.stream_thread = Thread(target=self._stream_audio)
+        self.stream_thread.start()
+    
+    def stop_recording(self):
+        self.running = False
+        self.stream_thread.join()
+        self.audio_stream.stop_stream()
+        self.audio_stream.close()
+        self.audio.terminate()
+
+if __name__ == "__main__":
+    logger.info("Testing Recorder")
+    q = Queue()
+    r = Recorder(q)
+    r.start_recording()
+    input("Press Enter to stop recording")
+    r.stop_recording()
+    logger.info(f"Queue size: {q.qsize()}")
---
+++ app/vad.py
@@ -0,0 +1,82 @@
+import onnxruntime
+import numpy as np
+from logging import getLogger, basicConfig, INFO
+
+logger = getLogger(__name__)
+basicConfig(level=INFO)
+
+class VAD():
+    def __init__(self, 
+                 model_path,
+                 device="cpu"):
+        logger.info(f"Loading VAD model from {model_path}")
+        opts = onnxruntime.SessionOptions()
+        opts.inter_op_num_threads = 1
+        if device == "cpu":
+            self.session = onnxruntime.InferenceSession(model_path, providers=["CPUExecutionProvider"],sess_options=opts)
+        else:
+            self.session = onnxruntime.InferenceSession(model_path, sess_options=opts)
+        self.sr = 16000
+        self._reset_states()
+    
+    def _validate_input(self, x:np.ndarray, sr: int):
+        if len(x.shape) == 1:
+            x = x.reshape(1, -1)
+        if len(x.shape) > 2:
+            raise ValueError(f"Too many dimensions for input audio chunk {x.dim()}")
+
+        if sr != 16000 and (sr % 16000 == 0):
+            step = sr // 16000
+            x = x[:,::step]
+            sr = 16000
+        if sr / x.shape[1] > 31.25:
+            raise ValueError("Input audio chunk is too short")
+
+        return x, sr
+    
+    def _reset_states(self, batch_size=1):
+        self._state = np.zeros((2, batch_size, 128),dtype=np.float32)
+        self._context = np.zeros(0,dtype=np.float32)
+        self._last_sr = 0
+        self._last_batch_size = 0
+    
+    def __call__(self, x:np.ndarray, sr:int):
+        x, sr = self._validate_input(x, sr)
+        num_samples = 512 if sr == 16000 else 256
+
+        if x.shape[-1] != num_samples:
+            raise ValueError(f"Provided number of samples is {x.shape[-1]} (Supported values: 256 for 8000 sample rate, 512 for 16000)")
+
+        batch_size = x.shape[0]
+        context_size = 64 if sr == 16000 else 32
+
+        if not self._last_batch_size:
+            self._reset_states(batch_size)
+        if (self._last_sr) and (self._last_sr != sr):
+            self._reset_states(batch_size)
+        if (self._last_batch_size) and (self._last_batch_size != batch_size):
+            self._reset_states(batch_size)
+
+        if not len(self._context):
+            self._context = np.zeros((batch_size, context_size), dtype=np.float32)
+
+        x = np.concatenate([self._context, x], axis=1)
+        if sr in [8000, 16000]:
+            ort_inputs = {'input': x, 'state': self._state, 'sr': np.array(sr, dtype='int64')}
+            ort_outs = self.session.run(None, ort_inputs)
+            out, self._state = ort_outs
+        else:
+            raise ValueError()
+
+        self._context = x[..., -context_size:]
+        self._last_sr = sr
+        self._last_batch_size = batch_size
+
+        return out
+
+if __name__ == "__main__":
+    logger.info("Testing VAD")
+    vad = VAD("model/vad/model.onnx")
+    x = np.random.randn(1, 512)
+    out = vad(x, 16000)
+    print(out)
