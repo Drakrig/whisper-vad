a88868c60143e512099c9366791bc273ba0efd83
drakrig <drakrig@gmail.com>
2025-01-10 19:12:05 +0300

    1. Renames whisper_wrapper.py to whisper.py 2. Renames S2T to WhisperWrapper 3. Add logging to whisper.py 4. Change __init__ for WhisperWrapper 5. Remove unused methods in whisper.py

--- app/whisper.py
+++ app/whisper.py
@@ -1,17 +1,10 @@
 from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
 import torch
 import numpy as np
-from threading import Thread
-import logging
-from logger_setup import setup_custom_logger
 
-logger = setup_custom_logger(__name__, 'logs/whisper.log', log_level=logging.INFO) 
-
-class S2T():
+class WhisperWrapper():
     def __init__(self, 
-                 model_path:str,
-                 input_queue:Queue,
-                 output_queue:Queue):
+                 model_path:str):
         logger.info(f"Loading model from {model_path}")
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
         torch_dtype = torch.float16 if self.device == "cuda" else torch.float32
@@ -31,9 +24,7 @@ class S2T():
             feature_extractor=self.processor.feature_extractor,
             torch_dtype=torch_dtype,
             device=self.device)
-        
-        self.input_queue = input_queue
-        self.output_queue = output_queue
+    
         
         logger.info("Pipeline ready")
 
@@ -50,20 +41,5 @@ class S2T():
             data = self.input_queue.get()
             text = self(data)
             self.output_queue.put(text)
-    
-    def start_whisper(self):
-        self.running = True
-        self.thread = Thread(target=self.process_audio)
-        self.thread.start()
-        
-    def stop_whisper(self):
-        self.running = False
         self.thread.join()
-
-if __name__ == "__main__":
-    logger.info("Testing Whisper")
-    in_q, out_q = Queue(), Queue()
-    s2t = S2T("app/model/whisper", in_q, out_q)
-    s2t.start_whisper()
-    input("Press Enter to stop whispering")
-    s2t.stop_whisper()
+        logger.info("Whisper stopped")
